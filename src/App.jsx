430bf085e62c8c4e17c15d33fdd6c7a61d55bfea
ani1911 <aniketshelke8487@gmail.com>
2025-07-11 14:08:40 +0530

    Initial commit-flimsage with vite

---
+++ src/App.jsx
@@ -0,0 +1,12 @@
+import './index.css'
+
+function App() {
+  return (
+    <div className="min-h-screen flex flex-col items-center justify-center bg-gray-100">
+      <h1 className="text-4xl font-bold text-blue-600">Welcome to FilmSage 🎬</h1>
+      <p className="mt-4 text-lg text-gray-700">Find movies by mood using GPT!</p>
+    </div>
+  )
+}
+
+export default App
